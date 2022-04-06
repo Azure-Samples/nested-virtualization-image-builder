794f626bd36b5508a72907f63822e755602c2d9d
Joshua Phelps <88395136+joshuaphelpsms@users.noreply.github.com>
2022-04-06 19:57:37 +0000

    update install location

--- images/windowsserver2022/scripts/install-vm-agent.ps1
+++ images/windowsserver2022/scripts/install-vm-agent.ps1
@@ -1,5 +1,5 @@
 mkdir "C:\Windows\OEM\GuestAgent"
-wget "https://github.com/Azure/WindowsVMAgent/releases/download/2.7.41491.1044/WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi" -UseBasicParsing -Outfile "C:\Windows\OEM\GuestAgent\WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi"
-Start-Process "C:\Windows\OEM\GuestAgent\WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi" /quiet
+wget "https://github.com/Azure/WindowsVMAgent/releases/download/2.7.41491.1044/WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi" -UseBasicParsing -Outfile "C:\Windows\OEM\GuestAgent\WindowsAzureVmAgent.fre.msi"
+Start-Process "C:\Windows\OEM\GuestAgent\WindowsAzureVmAgent.fre.msi" /quiet
 
 Write-Output "Done: install-vm-agent.ps1"
